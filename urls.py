d244c569d341cd9cc62f19ac3862f3040f0e14f6
tanishq ojha <tanishqojha75@gmail.com>
2025-10-24 21:42:41 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path("package/",views.package),
+    path("dashboard/",views.dashboard),
+    path("myblogs/",views.myblogs),
+    path("history/",views.history),
+    path("profile/",views.profile),
+    path("logout/",views.signout),
+]
+
